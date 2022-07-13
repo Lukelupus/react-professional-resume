29418cd66e6e21f0b5ef3a7c78491d14d467859a
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-13 20:50:26 -0300

    fixed navbar invalid hook calls. Added custom info and img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar"
+
 
 function App() {
-  return (
-    <div className="App">
-   
+  return (<>
+    <div>
+      <Navbar />
     </div>
-  );
+    </>);
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,8 @@
 import React from "react"
 import HomeIcon from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import SettingsIcon from '@mui/icons-material/Settings';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import ForwardIcon from '@mui/icons-material/Forward';
+import PersonIcon from '@mui/icons-material/Person';
+import faceImg from "../assets/logo.png"
+
 
 
 
@@ -15,31 +14,31 @@ function Navbar() {
                 <ul className="navbar-nav">
                     <li className="logo">
                         <a className="nav-link" href="/">
-                            <ForwardIcon className="icon logo-icon"/>
-                            <span className="link-text logo-text">Logo</span>
+                            <img className="icon" src={faceImg} alt="Lucas Developer"></img>
+                            <span className="link-text logo-text">Lucas M. Botelho</span>
                         </a>
                     </li>
                     <li className="nav-item">
                     <a href="/" className="nav-link">
-                        <HomeIcon className="icon" />
+                        <HomeIcon className="icon"/>
                         <span className="link-text">Home</span>
                     </a>
                     </li>
                     <li className="nav-item">
                     <a href="/" className="nav-link">
-                        <AccountCircleIcon className="icon"/>
-                        <span className="link-text">Login</span>
+                       <PersonIcon className="icon"/>
+                        <span className="link-text">Perfil</span>
                     </a>
                     </li>
                     <li className="nav-item">
                     <a href="/" className="nav-link">
-                        <MenuBookIcon className="icon" />
-                        <span className="link-text">Blog</span>
+
+                         <span className="link-text">Blog</span>
                     </a>
                     </li>
                     <li className="nav-item">
                     <a href="/" className="nav-link">
-                        <SettingsIcon className="icon" />
+                       
                         <span className="link-text">Settings</span>
                     </a>
                     </li>
