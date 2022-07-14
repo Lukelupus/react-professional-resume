fa801991f21fccd9dfa938942f96f29830947d22
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-14 14:22:06 -0300

    new designed. landing page with edinburgh photos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,13 @@ import React from "react";
 function Home() {
     return (<>
         <div className="home-page">
-            <img src="images/merussia.png" alt="fullstack web developer"></img>
+            <img className="lucas-img" src="images/merussia-modified.png" alt="fullstack web developer"></img>
             <div className="home-presentation-card">
-                <h1 className="home-title">Sobre</h1>
-                <hr></hr>
-                <h3 className="home-subtitle">Desenvolvedor de Web fullstack</h3>
-                <p className="home-content">Profissional criativo, empolgado e cheio de energia! Adoro resolver problemas da maneira elegante e eficiente. Atualmente estou me especializando com o MERN stack - MongoDB,Express, React e Node -.</p>
+                <h1 className="home-title">Lucas Moreira Botelho</h1>
+                <h3 className="home-subtitle">Desenvolvedor de Web</h3>
             </div>
         </div>
+        <img className="landing-page-img" alt="scotland-street-art" src="images/white-blue.jpeg"></img>
             </>)
 }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 function Profile() {
     return (<>
-        <h1>This is the Profile</h1>
+          <div className="home-page">
+            <img src="images/merussia.png" alt="fullstack web developer"></img>
+            <div className="home-presentation-card">
+                <h1 className="home-title">Sobre</h1>
+                <hr></hr>
+                <h3 className="home-subtitle">Desenvolvedor de Web fullstack</h3>
+                <p className="home-content">Profissional criativo, empolgado e cheio de energia! Adoro resolver problemas da maneira elegante e eficiente. Atualmente estou me especializando com o MERN stack - MongoDB,Express, React e Node -.</p>
+            </div>
+        </div>
     </>)
 }
 
