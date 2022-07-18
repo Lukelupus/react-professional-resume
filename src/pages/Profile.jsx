baf8db28061ff64b072f83596003d5271f1ea8e1
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-18 10:20:47 -0300

    Fixed anchor tags hover state.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,8 +15,8 @@ function Profile() {
                     <h3 className="profile-subtitle">Um Desenvolvedor FullStack</h3>
                     <p className="profile-content">Eu trabalho atualmente na arquitetura de sites. Desde o design inicial, escrita dos textos, desenvolvimento do código e estruturação de servidores e bancos de dados!</p>
                     <p className="profile-content">Então se você quer colocar sua cara no mundo, sua empresa na rede, construir sua lojinha virtual ou apenas ter aquele site pra chamar de seu, corre aqui pra aba de serviços ou contato pra gente conversar 🤓!</p>
-                    <p className="profile-content">Já se você quiser dar uma olhadinha no meu trabalho, dá uma olhada aqui no meu <a href="https://github.com/lukelupus">portfólio</a> ou clica no bonquinho aqui.</p> 
-                    <GitHubIcon className="icon" />
+                    <p className="profile-content">Já se você quiser dar uma olhadinha no meu trabalho, dá uma olhada aqui no meu <a href="https://github.com/lukelupus" target="_blank" rel="noreferrer">portfólio</a> ou clica no gatinho aqui.</p> 
+                    <a href="https://github.com/lukelupus" target="_blank" rel="noreferrer"><GitHubIcon className="profile-icon" /></a>
                     <p className="profile-content"> Atualmente trabalho com o MERN Stack: MongoDB, Express, React e Node.</p>
                     <h3 className="profile-subtitle-service">Serviços</h3>
                 </div>
