f480672643d723e0ba1972a1132017af3380a033
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-21 18:08:11 -0300

    moved components to next.js component file. Removed Link component from react-router

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -3,9 +3,8 @@ import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 import PreviewIcon from '@mui/icons-material/Preview';
 import CodeIcon from '@mui/icons-material/Code';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-import { Link } from 'react-router-dom'
+
 
 
 
@@ -17,34 +16,34 @@ function Navbar() {
             <nav className="navbar">
                 <ul className="navbar-nav">
                     <li className="logo">
-                        <Link className="nav-link" to="/">
+                        <a className="nav-link" to="/">
                             <KeyboardDoubleArrowRightIcon className="icon logo-icon" />
                             <span className="logo-text link-text">Lucas Botelho</span>
-                        </Link>
+                        </a>
                     </li>
                     <li className="nav-item profile-icon-mobile">
-                    <Link to="/profile" className="nav-link">
+                    <a to="/profile" className="nav-link">
                        <PersonIcon className="icon "/>
                         <span className="link-text">Perfil</span>
-                    </Link>
+                    </a>
                     </li>
                     <li className="nav-item">
-                    <Link to="/skills" className="nav-link">
+                    <a to="/skills" className="nav-link">
                         <CodeIcon className="icon" />
                          <span className="link-text">Habilidades</span>
-                    </Link>
+                    </a>
                     </li>
                     <li className="nav-item">
-                    <Link to="/services" className="nav-link">
+                    <a to="/services" className="nav-link">
                         <PreviewIcon className="icon" />
                         <span className="link-text">Serviços</span>
-                    </Link>
+                    </a>
                     </li>
                     <li className="nav-item profile-icon-mobile">
-                    <Link to="/" className="nav-link">
+                    <a to="/" className="nav-link">
                        <HomeIcon className="icon "/>
                         <span className="link-text">Home</span>
-                    </Link>
+                    </a>
                     </li>
                 </ul>
             </nav>
