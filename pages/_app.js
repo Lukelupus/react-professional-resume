c72e7dd4e9a0c760ffeb78e725b73f9bc4981a36
Lucas Boti <luke.boti.lorken@gmail.com>
2022-07-26 10:10:25 -0300

    fixed css animation timing and profile img position

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/styles.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head><meta name="viewport" content="width=device-width, initial-scale=1" /></Head>
+      <Component {...pageProps} />
+    </>)
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,7 @@ import { Html, Head, Main, NextScript } from "next/document"
         return (
         <Html lang="en">
             <Head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
                 <meta name="theme-color" content="#000000" />
                 <meta
                 name="description"
